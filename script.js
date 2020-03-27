f8e8e19eefcd4ca8fc22e2e97f2eeffe18f8fb6a
Rob Bowman <robertbowman17@btinternet.com>
2020-03-27 14:05:23 +0000

    Added scenarios

--- script.js
+++ script.js
@@ -1,25 +1,78 @@
-var slideIndex = 1;
-
-function plusSlides(n) {
-  showSlides(slideIndex += n)
+function ucde_case_study_load(){
+  showScreen(1);
+  showPersona(1);
+  showScenario(1);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n)
+
+var screenIndex = 1;
+var personaIndex = 1;
+var scenarioIndex = 1;
+
+function plusScreens(n) {
+  showScreen(screenIndex += n)
+}
+function plusPersonas(n) {
+  showPersona(personaIndex += n)
+}
+function plusScenarios(n) {
+  showScenario(scenarioIndex +=n)
 }
 
-function showSlides(n) {
+function currentScreen(n) {
+  showScreen(screenIndex = n)
+}
+function currentPersona(n) {
+  showPersona(personaIndex = n)
+}
+function currentScenario(n) {
+  showScenario(scenarioIndex = n)
+}
+
+function showScreen(n) {
   var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("slide-btn");
-  if (n > slides.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = slides.length;}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var screens = document.getElementsByClassName("screen");
+  var dots = document.getElementsByClassName("screen-btn");
+  if (n > screens.length) {screenIndex = 1;}
+  if (n < 1) {screenIndex = screens.length;}
+  for (i = 0; i < screens.length; i++) {
+      screens[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  screens[screenIndex-1].style.display = "block";
+  dots[screenIndex-1].className += " active";
+}
+
+function showPersona(n) {
+  var i;
+  var personas = document.getElementsByClassName("persona");
+  var dots = document.getElementsByClassName("persona-btn");
+  if (n > personas.length) {personaIndex = 1;}
+  if (n < 1) {personaIndex = personas.length;}
+  for (i = 0; i < personas.length; i++) {
+      personas[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  personas[personaIndex-1].style.display = "block";
+  dots[personaIndex-1].className += " active";
+}
+
+function showScenario(n) {
+  var i;
+  var scenarios = document.getElementsByClassName("scenario");
+  var dots = document.getElementsByClassName("scenario-btn");
+  if (n > scenarios.length) {scenarioIndex = 1;}
+  if (n < 1) {scenarioIndex = scenarios.length;}
+  for (i = 0; i < scenarios.length; i++) {
+      scenarios[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  scenarios[scenarioIndex-1].style.display = "block";
+  dots[scenarioIndex-1].className += " active";
 }
