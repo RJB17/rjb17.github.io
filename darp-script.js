5e3bc32545b08a3c3249642d12d405255b165289
Rob Bowman <robertbowman17@btinternet.com>
2020-04-07 09:34:55 +0100

    darp case study

--- darp-script.js
+++ darp-script.js
@@ -1,39 +1,15 @@
 function darp_load() {
-  showPlot(1);
   showArticle(1);
 }
 
-var biPlotIndex = 1;
 var articleIndex = 1;
 
-function plusPlots(n) {
-  showPlot(biPlotIndex +=n)
-}
 function plusArticles(n) {
   showArticle(articleIndex += n)
 }
 
-function currentPlot(n){
-  showPlot(biPlotIndex = n)
-}
 function currentArticle(n){
   showArticle(articleIndex = n)
-}
-
-function showPlot(n){
-  var i;
-  var plots = document.getElementsByClassName("bi-plot");
-  var dots = document.getElementsByClassName("bi-plot-btn");
-  if (n > plots.length) {biPlotIndex = 1;}
-  if (n < 1) {biPlotIndex = plots.length;}
-  for (i = 0; i < plots.length; i++) {
-      plots[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  plots[biPlotIndex-1].style.display = "block";
-  dots[biPlotIndex-1].className += " active";
 }
 
 function showArticle(n){
